13bdd392a8e9be83e4b4ad75bbe587134dac2b17
Sushanth Kumar <168724066+SushanthKumar2010@users.noreply.github.com>
2026-02-08 10:20:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 if not GEMINI_API_KEY:
     raise RuntimeError("GEMINI_API_KEY not set")
 
-MODEL_NAME = "gemini-2.5-flash-lite"
+MODEL_NAME = "gemini-3-pro-preview"
 
 ALLOWED_BOARDS = {"ICSE", "CBSE", "SSLC"}
 
@@ -306,6 +306,7 @@ STRICT ANSWERING RULES (VERY IMPORTANT):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=10000)
+
 
 
 
